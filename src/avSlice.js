12027e0743c91929d3ae52fe6a456784e78c3562
nazarchukdana <nazarchuk.dana05@gmail.com>
2024-10-02 13:12:50 -0400

    init initState for avSlice, add incr and decravquantity, import avReducer, add div addons-sel

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,48 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "./assets/images/projector.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "./assets/images/speaker.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "./assets/images/microphone.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "./assets/images/whiteboard.jpg",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: "./assets/images/signage.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
+        const item = state[action.payload];
+        if (item) item.quantity++;
       
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if(item && item.quantity > 0) item.quantity--;
     },
   },
 });
